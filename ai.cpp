74111203a4262cec4203552ac1d59228d9f216de
Pär Bohrarper <bohrarper@spotify.com>
2014-11-28 23:29:38 +0100

    Update to competition api

--- ai.cpp
+++ ai.cpp
@@ -5,104 +5,7 @@
 namespace monkeymusic {
 namespace ai {
 
-web::json::value move(const web::json::value &current_game_state) {
-  // This is where you put your AI code!
-  // The AI looks at the current game state and decides
-  // the monkey's next move.
-
-  // Every game has a limited number of turns. Use every turn wisely!
-  auto remaining_number_of_turns = current_game_state.at(U("turns"));
-
-  // The level layout is a 2D-matrix (an array of arrays).
-  //
-  // Every element in the matrix is a string. The string tells you what
-  // s located at the corresponding position on the level.
-
-  // In the warmup challenge, your objective is to find all music items
-  // and deliver them to the eagerly waiting Spotify user.
-
-  // "empty": an empty tile, you can move here
-  // "monkey": your monkey, this is where you're currently at
-  // "song" / "album" / "playlist": a music item, go get them!
-  // "user": go here once you've picked up all music items
-
-  // Too easy for you? Good...
-
-  // The real fun begins when the warmup is over and the competition begins!
-  auto current_level_layout = current_game_state.at(U("layout"));
-
-  // This is an array of all music items you've currently picked up
-  auto picked_up_music_items = current_game_state.at(U("pickedUp"));
-
-  // The position attribute tells you where your monkey is
-  auto current_position_of_monkey = current_game_state.at(U("position"));
-
-  // Speaking of positions...
-
-  // X and Y coordinates can be confusing.
-  // Which way is up and which way is down?
-
-  // Here is an example explaining how coordinates work in
-  // Monkey Music Challenge:
-
-  // {
-  //   "layout": [["empty", "monkey"]
-  //              ["song",  "empty"]]
-  //   "position": [0, 1],
-  //   ...
-  // }
-
-  // The "position" attribute tells you the location of your monkey
-  // in the "layout" matrix. In this example, you're at layout[0][1].
-
-  // If you send { "command": "move", "direction": "down", ... }
-  // to the server, you'll get back:
-
-  // {
-  //   "layout": [["empty", "empty"]
-  //              ["song",  "monkey"]]
-  //   "position": [1, 1]
-  // }
-
-  // If you instead send { "command": "move", "direction": "left", ... }
-  // to the server, you'll get back:
-
-  // {
-  //   "layout": [["monkey", "empty"]
-  //              ["song",   "empty"]]
-  //   "position": [0, 0]
-  // }
-
-  // So what about picking stuff up then?
-
-  // It's simple!
-
-  // Just stand next to something you want to pick up and move towards it.
-
-  // For example, say our current game state looks like this:
-
-  // {
-  //   "layout": [["empty", "empty"]
-  //              ["song",  "monkey"]]
-  //   "position": [1, 1],
-  //   "pickedUp": []
-  // }
-
-  // When you send { "command": "move", "direction": "left", ... }
-  // to the server, you'll get back:
-
-  //   "layout": [["empty",  "empty"]
-  //              ["empty",  "monkey"]]
-  //   "position": [1, 1],
-  //   "pickedUp": ["song"],
-  //   ...
-  // }
-
-  // Instead of moving, your monkey successfully picked up the song!
-
-  // Got it? Sweet! This message will self destruct in five seconds...
-
-  // TODO: You may want to do something smarter here
+web::json::value random_direction() {
   auto moves = std::vector<std::string>{"up", "left", "down", "right"};
 
   std::random_device rd; // obtain a random number from hardware
@@ -110,6 +13,96 @@ web::json::value move(const web::json::value &current_game_state) {
   std::uniform_int_distribution<> distr(0, 3); // define the range
 
   return web::json::value::string(moves[distr(eng)]);
+}
+
+web::json::value move(const web::json::value &current_game_state) {
+  // The gameState looks like this:
+  //
+  // {
+  //
+  //   layout: [[String]],
+  //   remainingTurns: Number,
+  //   isGameOver: Boolean,
+  //
+  //   score: Number,
+  //   position: [Number, Number],
+  //   inventory: [String],
+  //   inventorySize: Number,
+  //   buffs: {String: Number}
+  // }
+
+
+  // This is an example of a possible game state:
+  //
+  // {
+  //   layout: [["empty", "monkey"],
+  //            ["empty", "song"]],
+  //   remainingTurns: 10,
+  //   isGameOver: false,
+  //
+  //   score: 3,
+  //   position: [0, 1],
+  //   inventory: ["trap"],
+  //   buffs: {"speedy": 3}
+  // }
+
+
+  // You control your monkey by sending one of the following commands to the server:
+  //
+  // {"command": "move", "direction": ("left"|"right"|"up"|"down")}
+  // {"command": "use", "item": ("banana"|"trap")}
+  // {"command": "idle"}
+  // Remember that you can only use items that you have in your inventory!
+
+
+  // Lets talk positions and directions.
+  //
+  // X and Y coordinates can be confusing.
+  // Which way is up and which way is down?
+  //
+  // Here is an example explaining how coordinates work in
+  // Monkey Music Challenge:
+  //
+  // {
+  //   "layout": [["empty", "monkey"]
+  //              ["song",  "empty"]]
+  //   "position": [0, 1],
+  //   ...
+  // }
+  //
+  // The "position" attribute tells you the location of your monkey
+  // in the "layout" matrix. In this example, you're at layout[0][1].
+
+
+  // Here's an example!
+  //
+  // If you send {"command": "move", "direction": "down"}
+  // to the server, you'll get back:
+  //
+  // {
+  //   "layout": [["empty", "empty"]
+  //              ["song",  "monkey"]]
+  //   "position": [1, 1]
+  // }
+  //
+  // If you instead send { "command": "move", "direction": "left"}
+  // to the server, you'll get back:
+  //
+  // {
+  //   "layout": [["monkey", "empty"]
+  //              ["song",   "empty"]]
+  //   "position": [0, 0]
+  // }
+
+
+  // Go to <TODO: url> for more info about the rules of Monkey Music Challenge!
+
+
+  // TODO: You may want to do something smarter here
+  web::json::value ret;
+  ret[U("command")] = web::json::value::string(U("move"));
+  ret[U("direction")] = random_direction();
+  return ret;
 }
 
 }  // namespace ai
--- main.cpp
+++ main.cpp
@@ -13,15 +13,20 @@ namespace monkeymusic {
 
 web::json::value post_to_server(const std::string &team_name,
                                 const web::json::value &command) {
-  auto task = pplx::create_task([&team_name] {
-      web::http::client::http_client client(U("http://warmup.monkeymusicchallenge.com/"));
-      web::uri_builder builder(U("/team"));
-      builder.append_path(team_name);
-      return client.request(web::http::methods::POST, builder.to_string());
+  auto task = pplx::create_task([&team_name, &command] {
+      web::http::client::http_client client(U("http://competition.monkeymusicchallenge.com/"));
+      web::uri_builder builder(U("/game"));
+      return client.request(web::http::methods::POST, builder.to_string(), command);
     }).then([=](web::http::http_response response) {
-        if (response.status_code() == web::http::status_codes::OK) {
+      if (response.status_code() == web::http::status_codes::OK) {
         return response.extract_json();
       }
+      std::cout << "Error! We seem to have trouble talking to the server..." << std::endl;
+      std::cout << "    The server replied with status code: " << response.status_code() << std::endl;
+      // Extract the value so we can print it
+      auto body = response.extract_json().get();
+      std::cout << "    " << body << std::endl;
+      // Return null value
       return pplx::create_task([] { return web::json::value(); });
     });
   return task.get();
@@ -29,49 +34,53 @@ web::json::value post_to_server(const std::string &team_name,
 
 void run(const std::string &team_name,
          const std::string &api_key,
+         const std::string &game_id,
          const std::function<web::json::value(const web::json::value &)> &ai) {
   // Make an initial post to start the game
   web::json::value new_game_command;
-  new_game_command[U("command")] = web::json::value::string(U("new game"));
+  new_game_command[U("command")] = web::json::value::string(U("join game"));
+  new_game_command[U("team")] = web::json::value::string(team_name);
   new_game_command[U("apiKey")] = web::json::value::string(api_key);
+  new_game_command[U("gameId")] = web::json::value::string(game_id);
 
   // The server replies with the initial game state
   auto current_game_state = post_to_server(team_name, new_game_command);
 
   // The current game state tells you if you have any turns left
-  while (current_game_state.at(U("turns")).as_integer() > 0) {
-    std::cout << "Remaining turns: " << current_game_state.at(U("turns")) << std::endl;
+  while (!current_game_state.is_null() && !current_game_state.at(U("isGameOver")).as_bool()) {
+    std::cout << "Remaining turns: " << current_game_state.at(U("remainingTurns")) << std::endl;
 
     // Use your AI to decide in which direction to move...
-    auto next_move_direction = ai(current_game_state);
+    auto next_command = ai(current_game_state);
 
-    // ...and send a new move command to the server
-    web::json::value next_move_command;
-    next_move_command[U("command")] = web::json::value::string(U("move"));
-    next_move_command[U("direction")] = next_move_direction;
-    next_move_command[U("apiKey")] = web::json::value::string(api_key);
+    // Don't forget to include your teamName, API key and the game ID
+    next_command[U("team")] = web::json::value::string(team_name);
+    next_command[U("apiKey")] = web::json::value::string(api_key);
+    next_command[U("gameId")] = web::json::value::string(game_id);
 
     // After sending your next move, you'll get the new game state back
-    current_game_state = post_to_server(team_name, next_move_command);
+    current_game_state = post_to_server(team_name, next_command);
   }
 
   // If the game is over, our server will tell you how you did
   // Go to http://warmup.monkeymusicchallenge.com/team/<your-team-name> for more details
   std::cout << "Game over." << std::endl;
-  std::cout << "  " << current_game_state[U("message")] << std::endl;
 }
 
 }  // namespace monkeymusic
 
 int main(int argc, char* argv[]) {
   // Don't forget to provide the right command line arguments
-  if (argc < 3) {
-    std::cout << "Usage: monkeymusic <your-team-name> <your-api-key>" << std::endl;
+  if (argc < 4) {
+    std::cout << "Usage: monkeymusic <your-team-name> <your-api-key> <game-id>" << std::endl;
     if (argc < 1) {
       std::cout << "  Missing argument: <your-team-name>" << std::endl;
     }
     if (argc < 2) {
       std::cout << "  Missing argument: <your-api-key>" << std::endl;
+    }
+    if (argc < 3) {
+      std::cout << "  Missing argument: <game-id>" << std::endl;
     }
     return 1;
   }
@@ -79,9 +88,10 @@ int main(int argc, char* argv[]) {
   // You identify yourselves by your team name
   auto team_name = std::string(argv[1]);
   auto api_key = std::string(argv[2]);
+  auto game_id = std::string(argv[3]);
 
   try {
-    monkeymusic::run(team_name, api_key, monkeymusic::ai::move);
+    monkeymusic::run(team_name, api_key, game_id, monkeymusic::ai::move);
   } catch (const std::exception& ex) {
     std::cout << "Exception thrown: " << ex.what() << std::endl;
     return 1;
